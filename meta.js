43eb48db41de8ea7325e376fe7decf229704cfa2
yuxingyang <yuxingyang@tencent.com>
2017-05-08 19:10:36 +0800

    add vue-cli

---
+++ meta.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name",
+      "default": "vuemin"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A vue-base project starter"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    },
+    
+  },
+  "skipInterpolation": "{src/**/*.*}",
+  "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n npm run dev"
+};
